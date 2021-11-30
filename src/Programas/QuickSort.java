4de8e177b946fa62791d76ac6cd3cf16b2a45490
Alfre <Alfre@LAPTOP-5SB2Q671>
2021-11-30 15:13:39 -0600

    Se agrego el metodo de prueba de QuickSort en orden descendente

--- src/Programas/QuickSort.java
+++ src/Programas/QuickSort.java
@@ -29,6 +29,13 @@ public class QuickSort {
         for (int x : Q) {
             System.out.print(" " + x);
         }
+        System.out.println("");  
+        
+        System.out.println("Orden descendente");
+        QuicksortDes(Q, 0, Q.length - 1);
+        for (int x : Q) {
+            System.out.print(" " + x);
+        }
         System.out.println("");
 
 
@@ -73,5 +80,49 @@ public class QuickSort {
         if (j + 1 < der) {
             QuicksortAsc(numeros, j + 1, der);
         }
+        
     }
+        public static void QuicksortDes(int numeros[], int izq, int der) {
+        /*Este metodo recibe un arreglo de numero, y dos enteros que referencian el primer valor
+            Y el ultimo*/
+
+        //Se toma como pivote el primer valor
+        int pivote = numeros[izq];
+
+        //Se definen los dos lados y un auxiliar
+        int i = izq;
+        int j = der;
+        int aux;
+
+        while (i < j) {
+            while (numeros[i] >= pivote && i < j) {
+                i++;
+            }
+
+            while (numeros[j] < pivote) {
+                j--;
+            }
+
+            if (i < j) {
+
+                aux = numeros[i];
+                numeros[i] = numeros[j];
+                numeros[j] = aux;
+            }
+        }
+
+        numeros[izq] = numeros[j];
+        numeros[j] = pivote;
+
+        if (izq < j - 1) {
+            QuicksortDes(numeros, izq, j - 1);
+        }
+
+        if (j + 1 < der) {
+            QuicksortDes(numeros, j + 1, der);
+        }
+        ;
+    }
+
+    
 }
